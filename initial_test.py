d370cfbab071ec19adcf048cd9ee40a937de5a87
thegf4 <dunejagobind@gmail.com>
2021-05-11 13:56:28 +0400

    update initial_test.py
    Added a prefix (\) and gave 1 event

--- initial_test.py
+++ initial_test.py
@@ -1,2 +1,10 @@
 import discord
+from discord.ext import commands
 
+client  = commands.Bot(command_prefix = '\')
+
+@client.event
+async def on_ready():
+    print("Trudi's FINALLY ready")
+
+client.run("ODQwOTMxNTUyNDQzNDk4NTM2.YJfYiQ.dqyoly0dmS59Eypr-Nh7TYppM_I")
